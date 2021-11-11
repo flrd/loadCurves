d32a728b122a770e0db065d667b01cc997db673a
Markus Döring <m4rkus.doering@gmail.com>
2021-11-11 09:00:18 +0100

    delete unneeded files

--- R/LOCsegment.R
+++
@@ -1,9 +0,0 @@
-# LOCsegment <- function(marketLocation) {
-#   if (marketLocation == "" || is.null(marketLocation)) {
-#     marketLocation <- "{{Market Location}}"
-#   }
-#   
-#   LOC <- sprintf("LOC+172+%s'", marketLocation)
-#   
-#   return(LOC)
-# }
